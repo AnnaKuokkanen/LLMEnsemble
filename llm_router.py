bf6e877fb8401f4a9d7e5fbed1c960e9a3fc77b3
Kuokkanen Anna <anna.g.kuokkanen@helsinki.fi>
2025-09-26 11:04:34 +0300

    Changes to reflect particular use case

--- llm_router.py
+++ llm_router.py
@@ -2,116 +2,63 @@ import pandas as pd
 import numpy as np
 import torch
 import random
-import os.path
 from services.semantic_service import get_similarity_scores, find_k_nearest_neighbors, get_sentence_similarity
-from services.data_service import get_training_data
 from colorama import Fore, Back, Style, init
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, BertTokenizer, BertForSequenceClassification
 
-math_model = "Qwen/Qwen2-Math-1.5B"
-code_model = "Qwen/Qwen2.5-Coder-1.5B-Instruct"
-chat_model = "Qwen/Qwen1.5-1.8B-Chat"
-instruct_model = "Qwen/Qwen2.5-1.5B-Instruct"
-small_model = "Qwen/Qwen3-0.6B"
-finnish_model = "TurkuNLP/gpt3-finnish-xl"
+general_model = "meta-llama/Llama-3.2-3B"
+reasoning_model = "Qwen/Qwen2.5-VL-7B-Instruct"
+coding_model = "Qwen/Qwen2.5-Coder-3B-Instruct"
 
-#backup_model = "meta-llama/Llama-3.3-70B-Instruct"
+available_models = [general_model, reasoning_model, coding_model]
 
-available_models = [math_model, code_model, small_model]
+models_and_categories = zip(range(3), available_models)
 
-# method for finding best model for each query via semantic similarity of replies
-def train_knn(n):
-  df = get_training_data(n)
+# check if majority of nearest neighbors are coding-related, otherwise use binary BERT
+def knn_router(prompt, k=10):
 
-  # add placeholder columns for model name and similarity score
-  optimal_models = pd.DataFrame(np.empty((df.shape[0], 1)))
-  similarity_scores = pd.DataFrame(np.full((df.shape[0], 1), -999999))
-
-  df = pd.concat([df, optimal_models, similarity_scores], axis=1, ignore_index=True).rename(columns={0: "input", 1: "output", 2: "model_name", 3: "similarity_score"})
-
-  # for query in df, feed to available models and see which answer is the most semantically similar one to the output
-  for model_name in available_models:
-    print(Fore.MAGENTA + f"Next testing model {model_name}" + Style.RESET_ALL)
-
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCausalLM.from_pretrained(model_name)
-
-    if tokenizer.eos_token_id is None:
-      tokenizer.eos_token = ""
-    eos_token_id = tokenizer.eos_token_id
-
-    model.eval()
-
-    # do inference with chosen model for each query
-    for i, query in enumerate(df.loc[:, "input"]):
-      input_ids = tokenizer.encode(query, return_tensors="pt")
-      with torch.no_grad():
-        output_ids = model.generate(
-            input_ids,
-            #max_length=100,
-            num_return_sequences=1,
-            do_sample=True,  # Enable sampling for more creative results
-            #temperature=0.8,  # Controls randomness
-            #top_k=50,         # Optional: limit sampling to top-k tokens
-            #top_p=0.95        # Optional: nucleus sampling
-        )
-
-      generated_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
-
-      # get similarity score between generated text and ground truth
-      similarity_score = get_sentence_similarity(generated_text, df.loc[i, "output"])
-      print(similarity_score)
-      print(df.loc[i, "similarity_score"])
-
-      # if similarity score is better, update current model to
-      if similarity_score > df.loc[i, "similarity_score"]:
-        df.loc[i, "model_name"] = model_name
-        df.loc[i, "similarity_score"] = similarity_score
-
-      print(f"Completed training {i+1} / {df.shape[0]} queries")
-
-    # delete model from memory
-    del model
-
-  # write results to csv
-  df.to_csv("knn_results.csv", index=False)
-
-
-# routing strategy based on non-trained router
-def knn_router(query, k=1, n=10):
-  if not os.path.isfile("data/knn_results.csv"):
-    # if results file does not exist, train the router to obtain n data points
-    train_knn(n)
-
-  # read previous results
-  df = pd.read_csv("data/knn_results.csv")
-
-  # next we need to find k nearest neighbors in the embedding space in the training examples
-  # TODO find a way to store the embeddings, they only need to be calculated once, not on every inference run
-  corpus = df.loc[:, "input"].values.tolist()
+  print("Routing....")
+  # TODO calculate and store the embeddings beforehand, they only need to be calculated once, not on every inference run
+  df = pd.read_csv("./data/sample_questions.csv")
+  corpus = df.loc[:, "question"].values.tolist()
 
   # this is a list of row ids with the semantically nearest queries
-  knn_results = find_k_nearest_neighbors(query, corpus, k)
-  row_ids = [entry["corpus_id"] for entry in knn_results[0]]
-  print(knn_results)
+  neighbors = find_k_nearest_neighbors(prompt, corpus, k)
+  row_ids = [entry["corpus_id"] for entry in neighbors[0]]
+  print(neighbors)
 
   # see what is the most frequent model in the nearest neighbors and choose that (majority vote for now)
-  model_candidates = df.loc[row_ids, "model_name"]
-  print(Fore.RED + f"Model candidates: {model_candidates.to_list()}" + Style.RESET_ALL)
+  neighbor_categories = df.loc[row_ids, "category"]
+  print(Fore.RED + f"Neighbor categories: {neighbor_categories.to_list()}" + Style.RESET_ALL)
 
-  # majority vote
-  best_model = max(set(model_candidates), key=model_candidates.count)
-  return best_model
+  # majority vote, if question resembles coding, return the coder model
+  majority_category = max(set(neighbor_categories), key=neighbor_categories.count)
+  if majority_category == 2:
+    return coding_model
+  
+  # otherwise use a binary classifier
+  return bert_router(prompt, "/.models/bert_finetuned_binary/checkpoint-470")
+    
+
+# routing strategy based on trained model router
+def bert_router(prompt, model_path):
+  # load appropriate model
+  tokenizer = BertTokenizer.from_pretrained(model_path)
+  model = BertForSequenceClassification.from_pretrained(model_path)
+
+  # Tokenize the input
+  inputs = tokenizer(prompt, return_tensors="pt")
+
+  # Run inference (no gradients needed)
+  with torch.no_grad():
+      outputs = model(**inputs)
+
+  # Get predicted class (for classification tasks)
+  logits = outputs.logits
+  predicted_class = torch.argmax(logits, dim=1)
+
+  return models_and_categories.get(predicted_class)
 
 # random router for benchmarking purposes
 def random_router(query):
   return random.choice(available_models)
-
-
-# routing strategy based on trained model router
-def model_router(query, router_model):
-  # check if a trained model router exists on device
-
-  # if model does not exist, output
-  if router_model == None:
-    print("Please provide a trained router model")
--- llm_server.py
+++ llm_server.py
@@ -1,145 +1,59 @@
-import os.path
 import requests
+from sys import argv
 import torch
-import time
 import numpy as np
 import pandas as pd
 from csv import writer, reader
-from llm_router import knn_router
+from llm_router import knn_router, bert_router
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from colorama import Fore, Back, Style, init
 
-token = "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"
-API_URL = "https://api-inference.huggingface.co/models/{model_name}/"
+# Function call to router based on routing strategy
+def llm_router(prompt, routing_strat="inc_knn"):
+  # RAG to add context
 
-parameters = {
-  "max_new_tokens": 300,
-  "temperature": 0.01,
-  "return_full_text": False
-}
+  # determine if prompt is relevant by searching the RAG base and/or comparing it to sample questions
+  """ if min_distance > hyperparameter:
+        return "I apologize, the question does not seem relevant to our operations"
+      
+      if top_docs == None:
+        return "I could not find recent information on this" """
 
-headers = {
-  'Authorization': f'Bearer {token}',
-  'Content-Type': 'application/json'
-}
+  # two possible router options for now
+  model_name  = knn_router(prompt) if routing_strat == "inc_knn" else bert_router(prompt, "/.models/bert_finetuned_multi/checkpoint-309")
+  return generate(prompt, model_name)
 
-def initialize(path="results.csv"):
-  # create a csv file for storing results 
-  if not os.path.isfile(path):
-    with open("results.csv", "w") as f:
-      writer_object = writer(f)
-      writer_object.writerow(["query", "model", "logit_1", "logit_2", "logit_3", "mean_logits", "tokenized_length", "latency", "response"])
-      f.close()
+def generate(prompt, model_name):
+  tokenizer = AutoTokenizer.from_pretrained(model_name)
+  model = AutoModelForCausalLM.from_pretrained(model_name)
 
-def record_results(query, model_name, logits, mean_logits, token_representation_length, response, latency):
-  # store query, the model that was chosen and some metrics for future reference
+  tokenizer.pad_token_id = tokenizer.eos_token_id
 
-  # Check if the same query has been answered before 
-  df = pd.read_csv("results.csv")
-  matching_row = df[df["query"] == query]
+  # Move model to GPU if available
+  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+  model.to(device)
   
-  # if has not ben answered or we are not as confident in the answer, record
-  if matching_row.empty:
-    with open("results.csv", "a") as f:
-      writer_object = writer(f)
-      # if the same query has been answered, record results
-      writer_object.writerow([query, model_name, logits[0], logits[1], logits[2], mean_logits, token_representation_length, latency, response])
-      f.close()
-  elif np.mean(matching_row.iloc[0, :]['logit_1':'logit_3'].values) < sum(logits[:3])/3 and matching_row.iloc[0, :]['tokenized_length'] >= token_representation_length:
-    # replace row with updated information
-    index = matching_row.index[0]
-    df.iloc[index, :] = {"query": query, "model": model_name, "logit_1": logits[0], "logit_2": logits[1], "logit_3": logits[2], "mean_logits": mean_logits, "tokenized_length": token_representation_length, "latency": latency, "response": response}
-    df.to_csv("results.csv", index=False)
+  model.eval()
 
-def get_response(query, temperature=1.0):
-  # get the potential models
-  initialize()
-  potential_models = knn_router(query, k=3, n=10)
-  print(f"Potential models: {potential_models} \n")
+  input_ids = tokenizer.encode(prompt, return_tensors="pt")
 
-  # start measuring latency of generation process
-  start = time.time()
+  output_ids = model.generate(
+      **input_ids,
+      max_length=100,
+      num_return_sequences=1,
+      do_sample=True,
+      temperature=0.8,
+      #top_k=50,
+      #top_p=0.95
+  )
 
-  # if multiple potential models, generate answers with all of them and record the best one 
-  for model_name in potential_models:
-    print(f"Chose model {model_name} \n")
-    
-    # Get pretrained model
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCausalLM.from_pretrained(model_name)
+  del model
 
-    # Determine EOS token 
-    if tokenizer.eos_token_id is None:
-      tokenizer.eos_token = ""
-    eos_token_id = tokenizer.eos_token_id
+  answer = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+  return answer
 
-    # Move model to GPU if available
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.to(device)
 
-    # Tokenize user prompt
-    inputs = tokenizer(query, return_tensors="pt").to(device)
-    input_ids = inputs["input_ids"][0]  # Remove batch dimension
+def main():
+    llm_router(argv[0])
 
-    # Convert token IDs to token strings
-    token_strings = [tokenizer.decode([token_id]) for token_id in input_ids]
-    print(Fore.CYAN + f"Tokens as plain text: {token_strings} \n" + Style.RESET_ALL)
-
-    # Max number of tokens to generate
-    max_new_tokens = 100
-    # Context
-    generated_ids = inputs["input_ids"]
-    logits_history = []
-
-    for _ in range(max_new_tokens):
-      with torch.no_grad():
-        outputs = model(input_ids=generated_ids)
-        logits = outputs.logits
-        logits /= temperature
-
-      # Get the logits for the next token (last position)
-      next_token_logits = logits[:, -1, :]  # shape: (batch_size, vocab_size)
-      logits_history.append(torch.max(next_token_logits).item())
-
-      # Choose next token with max logit score
-      next_token_id = torch.argmax(next_token_logits, dim=-1).unsqueeze(-1)
-
-      # Exit if the end of a sequence
-      if next_token_id.item() == eos_token_id:
-        break
-
-      # Append next token to input for next step
-      generated_ids = torch.cat([generated_ids, next_token_id], dim=-1)
-
-    # Print information about logits
-    print(Fore.CYAN + Style.BRIGHT + f"Logits of chosen tokens: {logits_history} \n" + Style.RESET_ALL)
-    mean_logits = sum(logits_history) / len(logits_history)
-    print(f"Mean of logits: {mean_logits} \n")
-
-    # Decode chosen sequence
-    response_text = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
-    print(Fore.BLUE + Style.BRIGHT + f"Response by {model_name}: {response_text} \n" + Style.RESET_ALL)
-
-    # stop measuring latency of generation process
-    end = time.time()
-    latency = (end - start) * 1000
-    # write results 
-    record_results(query, model_name, logits_history, mean_logits, len(input_ids), response_text, latency)
-
-    # delete model from memory
-    del model
-    
-  df = pd.read_csv("results.csv")
-  response = df[df.loc[:, "query"] == query]["response"].values[0]
-  return response
-
-  """ else:
-    # Get large model from HuggingFace API
-
-    payload = {
-      "inputs": query,
-      "parameters": parameters
-    }
-
-    response = requests.post(API_URL.format(model_name=model_name), headers=headers, json=payload)
-    response_text = response.json()[0]['generated_text'].strip() """
+if __name__ == "__main__":
+    main()
